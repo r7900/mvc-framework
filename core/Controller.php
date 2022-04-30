f16c887d400d575bcdb4762c0dce8ab2dda75256
r7900 <srasfiaee@gmail.com>
2022-05-01 01:53:04 +0430

    Avoiding POST requests without csrf token
    Adding csrf protection into Controller class and avoiding POST requests without csrf token stored in session and form input .

--- core/Controller.php
+++ core/Controller.php
@@ -3,6 +3,7 @@
 namespace Core;
 
 use Core\View;
+use Core\Session;
 
 abstract class Controller
 {
@@ -12,5 +13,39 @@ abstract class Controller
     {
         $this->parameters = $param;
         $this->requestMethod = $method;
+
+
+        if ($method == 'POST') {
+            session_start();
+            if (!self::auth_csrf()) {
+                exit('invalid request');
+            }
+        }
+    }
+    private static function auth_csrf()
+    {
+        if (!Session::has('_token') || !isset($_POST['_token'])) {
+            return false;
+        }
+        if (time() > Session::get('token_expire') || Session::get('_token') !== $_POST['_token']) {
+            return false;
+        }
+        return true;
+    }
+
+
+    protected function csrfToken($expireTime = 5 * 60)
+    {
+        $token = bin2hex(random_bytes(32));
+        Session::set('_token', $token);
+        Session::set('token_expire', time() + $expireTime);
+        return $token;
+    }
+    protected function csrfInput($expireTime = 5 * 60)
+    {
+        $token = bin2hex(random_bytes(32));
+        Session::set('_token', $token);
+        Session::set('token_expire', time() + $expireTime);
+        return "<input type='hidden' name='_token' value='" . $token . "'>";
     }
 }
