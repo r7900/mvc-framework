1225dc608ac86a8ea1bd8efd68fbc8e62060e2e5
r7900 <srasfiaee@gmail.com>
2022-07-21 15:45:25 +0430

    Add composer autoloader
    It will fix include errors in linux (due to case-sensitive directory and file names)

--- core/init.php
+++ core/init.php
@@ -1,15 +1,16 @@
 <?php
 
 require_once "../app/config.php";
+require ROOT . '/vendor/autoload.php';
 require_once "Router.php";
 
-
+/*
 spl_autoload_register(function ($classname) {
     //echo $classname.'<br>';
     //echo APP_ROOT.'/'.$classname.'.php'."<br>";
     include str_replace('\\', '/', '../' . $classname . '.php');
 });
-
+*/
 require_once "../app/route.php";
 
 //echo "<pre>";
