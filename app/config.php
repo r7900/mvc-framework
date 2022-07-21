19b598b5eb4a67099baf8dab5880d8f36b690f2e
r7900 <srasfiaee@gmail.com>
2022-07-21 10:51:42 +0430

    Add error logging

--- app/config.php
+++ app/config.php
@@ -7,4 +7,15 @@ const DB_PASS = "";
 
 const SITE_URL = '';
 
+/// set DEV_ENV to 0 in production environment , it will log errors instead of showing them publicly
+const DEV_ENV = 1;
+
 define("APP_ROOT", dirname(__FILE__));
+define("ROOT", dirname(__FILE__, 2));
+
+
+
+error_reporting(E_ALL);
+ini_set('display_errors', DEV_ENV);
+ini_set('log_errors', !DEV_ENV);
+ini_set('error_log', ROOT . '/logs/errors.log');
